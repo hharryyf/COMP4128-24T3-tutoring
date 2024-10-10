3205c36b83d3738718ea754145630b634168f68b
hharryyf <harryheyifan99@gmail.com>
2024-10-10 13:27:41 +1100

    update

--- Week5/ehab.cpp
+++ Week5/ehab.cpp
@@ -56,7 +56,59 @@ int visited[MAX_SIZE];
 int f[MAX_SIZE], depth[MAX_SIZE];
 
 void dfs(int v, int p) {
-    
+    visited[v] = 1;
+    f[v] = p;
+    depth[v] = depth[p] + 1;
+    // back vertex
+    // we are looking for a backedge between nv and v
+    // we want to store the minimal cycle between nv and v
+    int back_v = -1;
+    for (auto &nv : g[v]) {
+        if (nv != p) {
+            if (visited[nv]) {
+                if (back_v == -1 || depth[back_v] < depth[nv]) {
+                    back_v = nv;
+                } 
+            }
+        }
+    }
+
+    if (back_v != -1) {
+        vector<int> ret;
+        int curr = v;
+        while (curr != back_v) {
+            ret.push_back(curr);
+            curr = f[curr];
+        }
+
+        ret.push_back(curr);
+
+        if ((int) ret.size() <= K) {
+            // we found a simple cycle of size at most K
+            printf("2\n");
+            printf("%d\n", (int) ret.size());
+            for (auto &p : ret) {
+                printf("%d ", p);
+            }
+            printf("\n");
+        } else {
+            // we found a cycle of size greater than K
+            // we print out an independent set instead
+            int i, j;
+            printf("2\n");
+            for (i = 0, j = 0; i < (int) ret.size() && j < (K + 1) / 2; i = i + 2, ++j) {
+                printf("%d ", ret[i]);
+            }
+            printf("\n");
+        }
+        exit(0);
+    }
+
+    for (auto &nv : g[v]) {
+        if (!visited[nv]) {
+            dfs(nv, v);
+        }
+    }
 }
 
 int main() {
@@ -71,6 +123,24 @@ int main() {
 
     dfs(1, 0);
 
-    
+    vector<int> odd, even;
+    for (i = 1; i <= N; ++i) {
+        if (depth[i] % 2 == 1) {
+            odd.push_back(i);
+        } else {
+            even.push_back(i);
+        }
+    }
+
+    if (odd.size() < even.size()) {
+        swap(odd, even);
+    }
+
+    // the tree case, the graph has no cycle
+    printf("2\n");
+    for (i = 0, j = 0; i < (int) odd.size() && j < (K + 1) / 2; ++i, ++j) {
+        printf("%d ", odd[i]);
+    }
+    printf("\n");
     return 0;
 }
--- Week5/shichikuji.cpp
+++ Week5/shichikuji.cpp
@@ -72,5 +72,38 @@ int main() {
     for (i = 1; i <= N; ++i) scanf("%lld", &c[i]);
     for (i = 1; i <= N; ++i) scanf("%lld", &k[i]);
     
+    for (i = 1; i <= N; ++i) {
+        edg.push_back(edge{0, i, c[i]});
+        for (j = i + 1; j <= N; ++j) {
+            edg.push_back(edge{i, j, (k[i] + k[j]) * (abs(x[i] - x[j]) + abs(y[i] - y[j]))});
+        }
+    }
+
+    sort(edg.begin(), edg.end());
+
+    for (auto &e : edg) {
+        if (find(e.u) != find(e.v)) {
+            // adding this edge will not create a cycle
+            // because u and v are from different connected components
+            ans += e.w;
+            unionset(e.u, e.v);
+            if (e.u == 0 || e.v == 0) {
+                pw.push_back(e.u + e.v);
+            } else {
+                conn.emplace_back(e.u, e.v);
+            }
+        }
+    }
+
+    printf("%lld\n", ans);
+    printf("%d\n", (int) pw.size());
+    for (auto &p : pw) {
+        printf("%d ", p);
+    }
+
+    printf("\n%d\n", (int) conn.size());
+    for (auto &p : conn) {
+        printf("%d %d\n", p.first, p.second);
+    }
     return 0;
 }
